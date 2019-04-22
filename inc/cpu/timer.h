a120f9d4dc3ef239e027124e9b66da3c9f1bef33
tearth <tearthdev@gmail.com>
2019-04-22 02:14:20 +0200

    Add function to enable timer.

--- inc/cpu/timer.h
+++ inc/cpu/timer.h
@@ -13,13 +13,14 @@ typedef struct TIMER_Definition
 
 	void (*TimerClockCmd)(uint32_t, FunctionalState);
 	uint32_t TimerClock;
+	uint8_t IRQChannel;
 } TIMER_Definition;
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
-bool TIMER_Enable(TIM_TypeDef timx);
+bool TIMER_Enable(TIM_TypeDef *timx, unsigned int milliseconds);
 TIMER_Definition *TIMER_GetDefinition(TIM_TypeDef *usartx);
 
 #ifdef __cplusplus
--- src/cpu/timer.c
+++ src/cpu/timer.c
@@ -7,72 +7,113 @@ TIMER_Definition TimerDefinitions[TIMERS_COUNT] =
 		.Definition = TIM1,
 		.TimerClockCmd = RCC_APB2PeriphClockCmd,
 		.TimerClock = RCC_APB2Periph_TIM1,
+		.IRQChannel = TIM1_CC_IRQn
 	},
 	{
 		.Id = 1,
 		.Definition = TIM2,
 		.TimerClockCmd = RCC_APB1PeriphClockCmd,
 		.TimerClock = RCC_APB1Periph_TIM2,
+		.IRQChannel = TIM2_IRQn
 	},
 	{
 		.Id = 2,
 		.Definition = TIM3,
 		.TimerClockCmd = RCC_APB1PeriphClockCmd,
 		.TimerClock = RCC_APB1Periph_TIM3,
+		.IRQChannel = TIM3_IRQn
 	},
 	{
 		.Id = 3,
 		.Definition = TIM4,
 		.TimerClockCmd = RCC_APB1PeriphClockCmd,
 		.TimerClock = RCC_APB1Periph_TIM4,
+		.IRQChannel = TIM4_IRQn
 	},
 	{
 		.Id = 4,
 		.Definition = TIM6,
 		.TimerClockCmd = RCC_APB1PeriphClockCmd,
 		.TimerClock = RCC_APB1Periph_TIM6,
+		.IRQChannel = TIM6_DAC1_IRQn
 	},
 	{
 		.Id = 5,
 		.Definition = TIM7,
 		.TimerClockCmd = RCC_APB1PeriphClockCmd,
 		.TimerClock = RCC_APB1Periph_TIM7,
+		.IRQChannel = TIM7_IRQn
 	},
 	{
 		.Id = 6,
 		.Definition = TIM8,
 		.TimerClockCmd = RCC_APB2PeriphClockCmd,
 		.TimerClock = RCC_APB2Periph_TIM8,
+		.IRQChannel = TIM8_UP_IRQn
 	},
 	{
 		.Id = 7,
 		.Definition = TIM15,
 		.TimerClockCmd = RCC_APB2PeriphClockCmd,
 		.TimerClock = RCC_APB2Periph_TIM15,
+		.IRQChannel = TIM1_BRK_TIM15_IRQn
 	},
 	{
 		.Id = 8,
 		.Definition = TIM16,
 		.TimerClockCmd = RCC_APB2PeriphClockCmd,
 		.TimerClock = RCC_APB2Periph_TIM16,
+		.IRQChannel = TIM1_UP_TIM16_IRQn
 	},
 	{
 		.Id = 9,
 		.Definition = TIM17,
 		.TimerClockCmd = RCC_APB2PeriphClockCmd,
 		.TimerClock = RCC_APB2Periph_TIM17,
+		.IRQChannel = TIM1_TRG_COM_TIM17_IRQn
 	},
 	{
 		.Id = 10,
 		.Definition = TIM20,
 		.TimerClockCmd = RCC_APB2PeriphClockCmd,
 		.TimerClock = RCC_APB2Periph_TIM20,
+		.IRQChannel = TIM20_UP_IRQn
 	},
 };
 
-bool TIMER_Enable(TIM_TypeDef timx)
-{
+bool timers_enabled[TIMERS_COUNT];
 
+bool TIMER_Enable(TIM_TypeDef *timx, unsigned int milliseconds)
+{
+	TIMER_Definition *definition;
+	TIM_TimeBaseInitTypeDef tim;
+	NVIC_InitTypeDef nvic;
+
+	if(definition = TIMER_GetDefinition(timx), definition == 0)
+	{
+		return false;
+	}
+
+	if(timers_enabled[definition->Id]) return false;
+
+	definition->TimerClockCmd(definition->TimerClock, ENABLE);
+
+	TIM_TimeBaseStructInit(&tim);
+	tim.TIM_CounterMode = TIM_CounterMode_Up;
+	tim.TIM_Prescaler = (SystemCoreClock / 2000) - 1;
+	tim.TIM_Period = (milliseconds * 2) - 1;
+	TIM_TimeBaseInit(timx, &tim);
+
+	TIM_ITConfig(timx, TIM_IT_Update, ENABLE);
+	TIM_Cmd(timx, ENABLE);
+
+	nvic.NVIC_IRQChannel = definition->IRQChannel;
+	nvic.NVIC_IRQChannelPreemptionPriority = 0;
+	nvic.NVIC_IRQChannelSubPriority = 0;
+	nvic.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&nvic);
+
+	return true;
 }
 
 TIMER_Definition *TIMER_GetDefinition(TIM_TypeDef *timx)
