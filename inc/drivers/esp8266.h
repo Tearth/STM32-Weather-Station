1fc23187414d70a43daf7bb2d994ed74797de655
tearth <tearthdev@gmail.com>
2019-05-01 15:50:23 +0200

    Add function to send GET request.

--- inc/drivers/esp8266.h
+++ inc/drivers/esp8266.h
@@ -39,8 +39,9 @@ bool ESP8266_Connect(const char *ssid, const char *password);
 bool ESP8266_Disconnect();
 bool ESP8266_GetIPAndMAC(char *ip, char *mac);
 int ESP8266_SendPing(char *target);
-bool ESP8266_SendGET(char *host, int port, char *target, char *responseBuf);
+bool ESP8266_SendGET(char *host, int port, char *path);
 bool ESP8266_OpenTCPConnection(char *host, int port);
 bool ESP8266_CloseTCPConnection();
+bool ESP8266_WaitForTCPClose();
 
 #endif
--- src/drivers/esp8266.c
+++ src/drivers/esp8266.c
@@ -21,7 +21,7 @@ int ESP8266_SendCommand(const char *str)
 	return USART_SendString(ESP8266_USART_INTERFACE, buffer);
 }
 
-int ESP8266_ReceiveData(char *buf)
+int ESP8266_ReceiveLine(char *buf)
 {
 	int length;
 	do
@@ -37,7 +37,7 @@ bool ESP8266_WaitForAck()
 	char buffer[128];
 	while(1)
 	{
-		ESP8266_ReceiveData(buffer);
+		ESP8266_ReceiveLine(buffer);
 		if(memcmp("OK\r\n", buffer, 4) == 0)
 		{
 			return true;
@@ -71,9 +71,9 @@ bool ESP8266_GetFirmware(ESP8266_FirmwareInfo* firmwareInfo)
 {
 	ESP8266_SendCommand("AT+GMR");
 
-	ESP8266_ReceiveData(firmwareInfo->ATVersionInfo);
-	ESP8266_ReceiveData(firmwareInfo->SDKVersionInfo);
-	ESP8266_ReceiveData(firmwareInfo->CompileTime);
+	ESP8266_ReceiveLine(firmwareInfo->ATVersionInfo);
+	ESP8266_ReceiveLine(firmwareInfo->SDKVersionInfo);
+	ESP8266_ReceiveLine(firmwareInfo->CompileTime);
 
 	return ESP8266_WaitForAck();
 }
@@ -107,8 +107,8 @@ bool ESP8266_GetIPAndMAC(char *ip, char *mac)
 	char line_ip[128], line_mac[128];
 
 	ESP8266_SendCommand("AT+CIFSR");
-	ESP8266_ReceiveData(line_ip);
-	ESP8266_ReceiveData(line_mac);
+	ESP8266_ReceiveLine(line_ip);
+	ESP8266_ReceiveLine(line_mac);
 
 	sscanf(line_ip, "+CIFSR:STAIP,\"%[^\"]\"\r\n", ip);
 	sscanf(line_mac, "+CIFSR:STAMAC,\"%[^\"]\"\r\n", mac);
@@ -124,7 +124,7 @@ int ESP8266_SendPing(char *target)
 	sprintf(buf, "AT+PING=\"%s\"", target);
 
 	ESP8266_SendCommand(buf);
-	ESP8266_ReceiveData(buf);
+	ESP8266_ReceiveLine(buf);
 
 	if(!ESP8266_WaitForAck())
 	{
@@ -135,9 +135,26 @@ int ESP8266_SendPing(char *target)
 	return ping;
 }
 
-bool ESP8266_SendGET(char *host, int port, char *target, char *responseBuf)
+bool ESP8266_SendGET(char *host, int port, char *path)
 {
-	return true;
+	if(!ESP8266_OpenTCPConnection(host, port)) return false;
+
+	char request_buffer[1024];
+	sprintf(request_buffer,
+			"GET %s HTTP/1.1\r\n"
+			"User-Agent: STM32 Weather Station\r\n"
+			"Host: %s\r\n"
+			"Connection: Close\r\n\r\n",
+			path, host);
+
+	char command_buffer[128];
+	sprintf(command_buffer, "AT+CIPSEND=%d", strlen(request_buffer));
+
+	ESP8266_SendCommand(command_buffer);
+	ESP8266_WaitForAck();
+
+	USART_SendString(ESP8266_USART_INTERFACE, request_buffer);
+	return ESP8266_WaitForTCPClose();
 }
 
 bool ESP8266_OpenTCPConnection(char *host, int port)
@@ -153,4 +170,21 @@ bool ESP8266_CloseTCPConnection()
 {
 	ESP8266_SendCommand("AT+CIPCLOSE");
 	return ESP8266_WaitForAck();
+}
+
+bool ESP8266_WaitForTCPClose()
+{
+	char buffer[8192];
+
+	while(1)
+	{
+		ESP8266_ReceiveLine(buffer);
+		printf(buffer);
+		if(strstr(buffer, "CLOSED\r\n") != NULL)
+		{
+			break;
+		}
+	}
+
+	return true;
 }
