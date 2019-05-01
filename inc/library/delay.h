901239c903e9aa61676d316f383b0cb8ba414103
tearth <tearthdev@gmail.com>
2019-05-02 01:41:28 +0200

    Add function to delay the specified amount of microseconds.

--- inc/library/delay.h
+++ inc/library/delay.h
@@ -3,6 +3,16 @@
 
 #include <cpu/sysclock.h>
 
+#define DelayMicroseconds(microseconds) do {\
+	asm volatile (\
+		"MOV R0,%[loops]\n\t"\
+		"1: \n\t"\
+		"SUB R0, #1\n\t"\
+		"CMP R0, #0\n\t"\
+		"BNE 1b \n\t" : : [loops] "r" (8*microseconds) : "memory"\
+		);\
+} while(0)
+
 #ifdef __cplusplus
  extern "C" {
 #endif
