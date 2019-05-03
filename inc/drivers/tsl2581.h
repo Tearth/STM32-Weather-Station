610f0791835e464b95ff0e73fba0f7f5559f92cb
tearth <tearthdev@gmail.com>
2019-05-03 02:07:06 +0200

    Add function to read register value from TSL.

--- inc/drivers/tsl2581.h
+++ inc/drivers/tsl2581.h
@@ -10,5 +10,6 @@ bool TSL2581_Enable();
 bool TSL2581_Disable();
 
 void TSL2581_WriteRegisterValue(uint8_t registerAddress, uint8_t registerValue);
+uint8_t TSL2581_ReadRegisterValue(uint8_t registerAddress);
 
 #endif
--- src/drivers/tsl2581.c
+++ src/drivers/tsl2581.c
@@ -37,6 +37,30 @@ void TSL2581_WriteRegisterValue(uint8_t registerAddress, uint8_t registerValue)
 	I2C_SendData(TSL2586_I2C_INTERFACE, registerValue);
 
 	while(I2C_GetFlagStatus(TSL2586_I2C_INTERFACE, I2C_ISR_TC) == RESET);
+	I2C_GenerateSTOP(TSL2586_I2C_INTERFACE, ENABLE);
+}
 
+uint8_t TSL2581_ReadRegisterValue(uint8_t registerAddress)
+{
+	while(I2C_GetFlagStatus(TSL2586_I2C_INTERFACE, I2C_ISR_BUSY) != RESET);
+
+	I2C_TransferHandling(TSL2586_I2C_INTERFACE, TSL2586_I2C_ADDRESS << 1, 1, I2C_SoftEnd_Mode, I2C_Generate_Start_Write);
+	while(I2C_GetFlagStatus(TSL2586_I2C_INTERFACE, I2C_ISR_TXIS) == RESET);
+
+	I2C_SendData(TSL2586_I2C_INTERFACE, 0x80 | registerAddress);
+
+	while(I2C_GetFlagStatus(TSL2586_I2C_INTERFACE, I2C_ISR_TC) == RESET);
+
+	I2C_TransferHandling(TSL2586_I2C_INTERFACE, TSL2586_I2C_ADDRESS << 1, 2, I2C_AutoEnd_Mode, I2C_Generate_Start_Read);
+
+	I2C_ReceiveData(TSL2586_I2C_INTERFACE);
+	while(I2C_GetFlagStatus(TSL2586_I2C_INTERFACE, I2C_ISR_RXNE) == RESET);
+
+	uint8_t value = I2C_ReceiveData(TSL2586_I2C_INTERFACE);
+	while(I2C_GetFlagStatus(TSL2586_I2C_INTERFACE, I2C_ISR_RXNE) == RESET);
+
+	while(I2C_GetFlagStatus(TSL2586_I2C_INTERFACE, I2C_ISR_STOPF) == RESET);
 	I2C_ClearFlag(TSL2586_I2C_INTERFACE, I2C_ICR_STOPCF);
+
+	return value;
 }
